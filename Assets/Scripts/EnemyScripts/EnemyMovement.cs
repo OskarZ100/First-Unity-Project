b3d3e851ecb04726d15e87685df9cd712b0be5fe
oskar <oskaraszinecnko500@gmail.com>
2026-07-27 18:58:37 -0400

    Made an enemy and allowed it to follow the player

---
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public float speed = 0.02f; 
+    void Update()
+    {
+        Debug.Log(speed);
+        //Using move towards
+        transform.position = Vector3.MoveTowards(
+            transform.position,  //the thing thats moving position 
+            player.transform.position,  //what you want to move towards
+            speed*Time.deltaTime    //how fast its moving 
+        );
+    }
+}
