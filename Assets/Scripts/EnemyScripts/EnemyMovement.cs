3ddfe0e0041c4a6c6e968b8bcb2cc060a59b7918
oskar <oskaraszinecnko500@gmail.com>
2026-08-12 22:18:52 -0400

    added 3 new types of enemies

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -1,14 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    
+    private Rigidbody rb;
     public float speed = 0.02f;
+    public int enemyType;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        System.Random rand = new System.Random();
+        enemyType = rand.Next(0, 3); 
+
+        // 0 = Normal
+        // 1 = jump
+        // 2 = SPEED
+        if(enemyType == 1)
+        {
+            StartCoroutine(Jump());
+
+            InvokeRepeating("Jump", 2f, 5f);
+        }else if(enemyType == 2)
+        {
+            Debug.Log("SPEED");
+
+            StartCoroutine(Speed()); 
+        }
+        
+
+
         player = GameManager.instance.player;
     } 
     void Update()
@@ -20,5 +44,30 @@ public class EnemyMovement : MonoBehaviour
             player.transform.position,  //what you want to move towards
             speed*Time.deltaTime    //how fast its moving 
         );
+    }
+
+    IEnumerator Jump()
+    {
+        while (true)
+        {
+            float oldSpeed = speed;
+            yield return new WaitForSeconds(7f);
+            rb.AddForce(Vector3.up * 20f, ForceMode.Impulse);
+            speed *= 1.3f;
+            yield return new WaitForSeconds(3f);
+            speed = oldSpeed;
+            
+        }
+    }
+
+    IEnumerator Speed()
+    {
+        while(true){
+            float oldSpeed = speed;
+            yield return new WaitForSeconds(5f);
+            speed *= 1.2f;
+            yield return new WaitForSeconds(2f);
+            speed = oldSpeed;
+        }
     }
 }
